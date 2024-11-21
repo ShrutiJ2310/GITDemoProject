8ac14aa5a7a711c283f582e9528dcbb18b992fbd
Shruti Jagtap <141856649+ShrutiJ2310@users.noreply.github.com>
2024-11-21 12:43:15 +0530

    Update DemoGITClass.java
    Taylor swift - 22

--- src/GITDemo/DemoGITClass.java
+++ src/GITDemo/DemoGITClass.java
@@ -4,6 +4,9 @@ public class DemoGITClass {
 	public static void main(String args[])
 	{
 		System.out.println("Hello world");
+		System.out.println("These are the changes that have been done by other developers");
+		int age =22;
+		System.out.println("I don't know about you, but I'm feeling "+age);
 	}
 
 }
